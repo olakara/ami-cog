c9466adb0e62a3cc1586c00183e1c34648b52c4f
Abdel Raoof <olakara@gmail.com>
2023-04-25 17:27:54 +0400

    chore: cleanup!

--- AmiCog.Api/Controllers/AuthenticationController.cs
+++ AmiCog.Api/Controllers/AuthenticationController.cs
@@ -29,10 +29,6 @@ namespace AmiCog.Api.Controllers
         [HttpPost("register")]
         public async Task<IActionResult> Register(RegisterRequest request)
         {
-            // var command = new RegisterCommand(request.FirstName,
-            //     request.LastName, 
-            //     request.Email,
-            //     request.Password);
             var command = _mapper.Map<RegisterCommand>(request);
             var registerResult = await _mediator.Send(command);
 
@@ -46,7 +42,6 @@ namespace AmiCog.Api.Controllers
         public async Task<IActionResult> Login(LoginRequest request)
         {
             
-            //var query = new LoginQuery(request.Email, request.Password);
             var query = _mapper.Map<LoginQuery>(request);
             var authResult = await _mediator.Send(query);
 
@@ -54,16 +49,6 @@ namespace AmiCog.Api.Controllers
                 result => Ok(_mapper.Map<AuthenticationResponse>(result)),
                 errors =>  Problem(errors)
             );
-        }
-        
-        private AuthenticationResponse MapAuthResult(AuthenticationResult result)
-        {
-            return new AuthenticationResponse(
-                result.User.Id, 
-                result.User.FirstName,
-                result.User.LastName,
-                result.User.Email,
-                result.Token);
         }
     }
 }
