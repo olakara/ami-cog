58d4acd7c85867cb2fce098f6258bf542d008668
Abdel Raoof <olakara@gmail.com>
2023-04-29 09:55:18 +0400

    feat: implement basic validation behavior

---
+++ AmiCog.Application/Common/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,17 @@
+﻿using AmiCog.Application.Authentication.Commands.Register;
+using AmiCog.Application.Authentication.Common;
+using ErrorOr;
+using MediatR;
+
+namespace AmiCog.Application.Common.Behaviors;
+
+public class ValidateRegisterCommandBehavior : IPipelineBehavior<RegisterCommand, ErrorOr<AuthenticationResult>>
+{
+    public async Task<ErrorOr<AuthenticationResult>> Handle(RegisterCommand request, 
+                                                        RequestHandlerDelegate<ErrorOr<AuthenticationResult>> next,
+                                                        CancellationToken cancellationToken)
+    {
+        var result = await next();
+        return result;
+    }
+}
--- AmiCog.Application/DependencyInjection.cs
+++ AmiCog.Application/DependencyInjection.cs
@@ -1,4 +1,8 @@
-﻿using MediatR;
+﻿using AmiCog.Application.Authentication.Commands.Register;
+using AmiCog.Application.Authentication.Common;
+using AmiCog.Application.Common.Behaviors;
+using MediatR;
+using ErrorOr;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace AmiCog.Application;
@@ -8,6 +12,9 @@ public static class DependencyInjection
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
+        services.AddScoped<
+                IPipelineBehavior<RegisterCommand, ErrorOr<AuthenticationResult>>,
+                ValidateRegisterCommandBehavior>();
         return services;
     }
 }
